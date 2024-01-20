04ef9971436a60b5b6a72c58c684064def88bc39
gitviini <gvinicius105@gmail.com>
2024-01-20 09:14:10 -0300

    up

--- app/users/urls.py
+++ app/users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', view=views.index, name='index'),
+    path('perfil/', view=views.perfil, name='perfil'),
     path('login/', view=views.login, name='login'),
-    path('sigin/', view=views.sigin, name='sigin'),
+    path('signup/', view=views.signup, name='signup'),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 from django.contrib.messages import constants
 from django.contrib import messages
 
-def login(req):
+def index(req):
+    return redirect('login')
 
+def login(req):
     if req.method == 'GET':
         pass
     else:
         name = req.POST['name']
         password = req.POST['password']
-
-        if User.objects.filter(username=name).exists():
-            pass
+        user = auth.authenticate(req, username=name, password=password)
+        if user:
+            try:
+                auth.login(req, user=user)
+                messages.add_message(
+                    req, constants.SUCCESS, f'user: {name} loged'
+                )
+                return redirect('perfil')
+            except:
+                messages.add_message(
+                    req, constants.ERROR, 'error in server'
+                )
         else:
-            pass
-        
-
+            messages.add_message(
+                req, level=constants.ERROR, message='name or password incorrect'
+            )
     return render(req, 'login.html')
 
-def sigin(req): 
+def signup(req): 
 
     if req.method == 'GET':
         pass
@@ -29,17 +41,29 @@ def sigin(req):
         confirm = req.POST['confirm']
 
         if password == confirm:                
-            try:
-                User.objects.create_user(
-                    username=name,
-                    password=password
-                )
-                messages.add_message(
-                    req, constants.SUCCESS, 'sigin sucess'
-                )
-            except:
-                messages.add_message(
-                    req, constants.ERROR, 'server failed'
-                )
+                if not User.objects.filter(username=name).exists():
+                    try:
+                        User.objects.create_user(
+                            username=name,
+                            password=password
+                        )
+                        messages.add_message(
+                            req, constants.SUCCESS, 'sigin sucess'
+                        )
+                    except:
+                        messages.add_message(
+                            req, constants.ERROR, 'server failed'
+                        )
+                else:
+                    messages.add_message(
+                        req, constants.ERROR, 'user exist'
+                    )
+        else:
+            messages.add_message(
+                req, constants.ERROR, 'passwords don´t match'
+            )
 
-    return render(req, 'sigin.html')
+    return render(req, 'signup.html')
+
+def perfil(req):
+    return render(req, template_name='perfil.html')
